214fb34fab65ace0f9d8b459d6263f750389e239
Aleksandr Semenov <alex.sergeevich1348@gmail.com>
2025-06-28 11:37:21 +0300

    add git-blame plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,6 +6,16 @@ require("lazy").setup({
   { "hrsh7th/cmp-nvim-lsp" },
   { "neovim/nvim-lspconfig" },
   {
+    "f-person/git-blame.nvim",
+    event = "VeryLazy",
+    opts = {
+        enabled = true,
+        message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+        date_format = "%m.%d.%Y %H:%M:%S",
+        virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
+    },
+  },
+  {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim" },
