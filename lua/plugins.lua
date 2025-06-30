9353f1f43399c90168323cc0d9b1ac5ea241113c
Aleksandr Semenov <alex.sergeevich1348@gmail.com>
2025-06-30 18:45:12 +0300

    remove tag from telescope installation

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -37,7 +37,6 @@ require("lazy").setup({
   },
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local telescope = require("telescope")
