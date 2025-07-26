6be7430141298e9179d96369099d5359e0a03738
Aleksandr Semenov <alex.sergeevich1348@gmail.com>
2025-07-26 12:33:54 +0300

    fix git-blame date

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -31,7 +31,7 @@ require("lazy").setup({
     opts = {
         enabled = true,
         message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
-        date_format = "%m.%d.%Y %H:%M:%S",
+        date_format = "%d.%m.%Y %H:%M:%S",
         virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
     },
   },
