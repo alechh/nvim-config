14406f978077271fea3ef0df2d60b46f8436d449
Aleksandr Semenov <alex.sergeevich1348@gmail.com>
2025-07-28 19:59:32 +0300

    change colorscheme to catppuccin-frape

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -94,18 +94,15 @@ require("lazy").setup({
   {
     "folke/tokyonight.nvim", enabled = false
   },
-  {
-  'projekt0n/github-nvim-theme',
-  name = 'github-theme',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-      require('github-theme').setup({
-      -- ...
-      })
+  { 
+	"catppuccin/nvim",
+	name = "catppuccin",
+    priority = 1000,
+	config = function()
+		require('catppuccin').setup({})
 
-      vim.cmd('colorscheme github_dark_dimmed')
-  end,
+		vim.cmd.colorscheme "catppuccin-frappe"
+	end,
   },
   {
     "akinsho/toggleterm.nvim",
@@ -165,7 +162,7 @@ require("lazy").setup({
     opts = function()
         return {
         options = {
-            theme = "github_dark_dimmed",
+            theme = "catppuccin",
             icons_enabled = true,
             section_separators = "",
             component_separators = "|",
